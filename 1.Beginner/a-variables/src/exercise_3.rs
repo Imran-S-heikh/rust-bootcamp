c552c56fe6b2ef6026858f58aa1cd652ccfc552f
Imran-S-heikh <imransheikhsadi@gmail.com>
2023-08-26 02:34:26 +0600

    1.Beginer: Variables

---
+++ 1.Beginner/a-variables/src/exercise_3.rs
@@ -0,0 +1,8 @@
+// Fix this code with shadowing
+
+pub fn run() {
+    let x = "three"; // don't change this line
+    println!("Spell a Number : {}", x);
+    let x = 3; // don't rename this variable
+    println!("Number plus two is : {}", x + 2);
+}
