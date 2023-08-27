6a49ae0972c399ba35cd0b420ecc71fb2e6b9462
Imran-S-heikh <imransheikhsadi@gmail.com>
2023-08-27 23:20:31 +0600

    Beginer: Functions

---
+++ 1.Beginner/d-functions/src/exercise_1.rs
@@ -0,0 +1,9 @@
+// Something's missing from the function definition. Fix it so it compiles!
+
+pub fn run (){
+    call_me(9395550113);
+}
+
+fn call_me(num: u64) {
+    println!("Ring! Call number {}", num);
+}
