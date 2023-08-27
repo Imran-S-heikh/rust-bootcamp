16244f72da4a2ab28e23572b882617d17a582efc
Imran-S-heikh <imransheikhsadi@gmail.com>
2023-08-27 23:01:02 +0600

    Beginer: Const and Static

---
+++ 1.Beginner/c-constants-statics/src/exercise_1.rs
@@ -0,0 +1,7 @@
+// Fix the code so it compiles!
+
+const NUMBER: u8 = 3;
+
+pub fn run (){
+    println!("Number {}", NUMBER);
+}
---
+++ 1.Beginner/c-constants-statics/src/main.rs
@@ -0,0 +1,7 @@
+pub mod exercise_1;
+pub mod exercise_2;
+
+fn main() {
+    exercise_1::run();
+    exercise_2::run();
+}
