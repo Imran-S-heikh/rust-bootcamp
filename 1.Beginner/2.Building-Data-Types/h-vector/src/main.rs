9e170bdc93c8be929a6099fe1245a165ffc0be60
Imran-S-heikh <imransheikhsadi@gmail.com>
2023-09-09 12:46:37 +0600

    Beginner(DataTypes): Vector

---
+++ 1.Beginner/2.Building-Data-Types/h-vector/src/main.rs
@@ -0,0 +1,13 @@
+pub mod exercise_0;
+pub mod exercise_1;
+pub mod exercise_2;
+pub mod exercise_3;
+pub mod exercise_4;
+
+fn main() {
+    exercise_0::main();
+    exercise_1::main();
+    exercise_2::main();
+    exercise_3::main();
+    exercise_4::main();
+}
