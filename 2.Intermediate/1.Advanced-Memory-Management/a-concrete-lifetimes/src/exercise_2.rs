6d1974de2c965de3c3335d6f460beb80cc5a6bd6
Imran-S-heikh <imransheikhsadi@gmail.com>
2023-09-28 23:08:25 +0600

    Intermediate(Advanced-Momory-Management): Concrete Lifetimes

---
+++ 2.Intermediate/1.Advanced-Memory-Management/a-concrete-lifetimes/src/exercise_2.rs
@@ -0,0 +1,10 @@
+// Fix the code by shifting only one statement.
+
+pub fn main() {
+    let mut my_str = "Old String".to_owned();
+    let ref1 = &my_str;
+    println!("{ref1}");
+    let ref2 = &mut my_str;
+    ref2.replace_range(0..3, "New");
+    println!("{ref2}");
+}
