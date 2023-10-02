14e25cc51bb4a175e3d42234ac9fecbe6ff01e32
Imran-S-heikh <imransheikhsadi@gmail.com>
2023-10-02 23:29:38 +0600

    Intermediate(Advanced-Memory-Management): Box Smart Pointer

---
+++ 2.Intermediate/1.Advanced-Memory-Management/d-box-smart-pointer/src/exercise_0.rs
@@ -0,0 +1,9 @@
+// Initialize heap_var to store value 4 on the heap & make the code execute successfully.
+
+pub fn main() {
+    let stack_var = 5;
+    // TODO: initialize this variable
+    let heap_var = Box::new(4);
+    let res = stack_var + *heap_var;
+    assert_eq!(res, 9);
+}
